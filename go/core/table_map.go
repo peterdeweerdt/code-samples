980fd230278ec18f5905971c4b4d10a4196a111b
Peter deWeerdt <peter@deweerdt.com>
2018-10-10 08:25:46 -0700

    Delete table_map.go

--- go/core/table_map.go
+++
@@ -1,7 +0,0 @@
-package core
-
-type TableMap struct {
-	BeaconID  string   `json:"beacon_id"`
-	SiteID    KountaID `json:"site_id"`
-	TableName string   `json:"table_id"` //todo: coordinate this rename with client apps
-}
